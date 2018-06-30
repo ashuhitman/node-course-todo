64d24f631cb18b89f109036d03a25d5955d4a91a
Ashu <ashu8306@gmail.com>
2018-07-01 02:14:43 +0530

    Add GET /users/me and authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -49,7 +49,24 @@ userSchema.methods.generateAuthToken = function () {
 
     return user.save().then(() => {
        return token;
+
     });
+};
+
+userSchema.statics.findByToken = function (token) {
+   var User = this;
+   var decoded;
+
+   try {
+     decoded = jwt.verify(token, 'abc123');
+   }catch(e) {
+      return Promise.reject(); 
+   }
+   return User.findOne({
+     '_id': decoded._id,
+     'tokens.token': token,
+     'tokens.access': 'auth'
+   });
 };
 
 var User = mongoose.model('Users', userSchema);
